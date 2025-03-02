292125ad104e836410a133a772ec61157df84793
Rutik <tarirutik@gmail.com>
2025-03-02 13:42:41 +0530

    SAS

---
+++ app/src/test/java/com/example/studentattendancesystem/ClassIitem.java
@@ -0,0 +1,27 @@
+package com.example.studentattendancesystem;
+
+public class ClassIitem {
+    private String className;
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    private String subjectName;
+
+    public ClassItem(String className, String subjectName) {
+        this.className = className;
+        this.subjectName = subjectName;
+    }
+}
